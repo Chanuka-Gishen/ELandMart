a223379d8a3a3cb51786b314437f1721370c8d13
charith2000 <66793787+charith2000@users.noreply.github.com>
2021-03-01 22:14:23 +0530

    Added Navigation Bar

--- src/components/common/NavigationBar.js
+++ src/components/common/NavigationBar.js
@@ -1,11 +1,78 @@
 import React,{Component} from "react";
+import logo from "../../assets/images/profile.jpg";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../../assets/css/styles.css";
+import "../../assets/css/vertical-navbar-with-menu-and-social-menu.css";
+import "../animations/AlertAnimation";
+import AlertAnimation from "../animations/AlertAnimation";
 export default class NavigationBar extends Component{
 
     render() {
         return (
             <div>
-                <h2>Navigation Bar</h2>
+                <div className="col-lg-3 d-flex flex-column justify-content-between px-0"
+                     id="header">
+                    <nav className="navbar navbar-expand-lg navbar-light d-flex flex-column px-0">
+                        <div id="m-navbar-brand"
+                             className="text-center">
+                            <a className="navbar-brand m-0 p-2"
+                               href="assets/img/profile.jpg"
+                               data-toggle="lightbox">
+                                <div style={{height:'500%',width:'50%',marginLeft:'auto',marginRight:'auto'}}>
+                                <AlertAnimation/>
+                                </div>
+                                    <h2 className="h5">
+                                        -E-Land-Mart-
+                                    </h2>
+                                    <h1 className="h6">
+                                        Team Algo Messiahs
+                                    </h1>
+                            </a>
+                            <button className="navbar-toggler"
+                                    type="button"
+                                    data-toggle="collapse"
+                                    data-target="#navbarSupportedContent"
+                                    aria-controls="navbarSupportedContent"
+                                    aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                        </div>
+                        <div id="m-navbar-menu"
+                             className="w-100">
+                            <div className="collapse navbar-collapse"
+                                 id="navbarSupportedContent">
+                                <ul className="navbar-nav d-flex flex-column w-100">
+                                    <li className="nav-item active">
+                                        <a className="nav-link font-weight-bold"
+                                           href="#">
+                                            About
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link font-weight-bold"
+                                           href="#">
+                                            Work
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link font-weight-bold"
+                                           href="#">
+                                            Portfolio
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link font-weight-bold"
+                                           href="#">
+                                            Contact
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+            </div>
             </div>
         );
     }
