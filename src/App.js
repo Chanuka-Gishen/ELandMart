daf014e992eed04a889b492f7f2b510df62ad5e2
kasunsene <66358910+kasunsene@users.noreply.github.com>
2021-03-17 11:36:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import NavigationBar from "./components/common/NavigationBar";
 import AlertAnimation from "./components/animations/AlertAnimation";
 import Login from "./components/Login";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import Validate from "./components/Validate";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
+  return (
+    <div className="App">
+      {/*<AlertAnimation/>*/}
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/Validate">Validate</Link>
+            </li>
+          </ul>
 
-            {/*<AlertAnimation/>*/}
-            <Router>
-            <Switch>
-                <Route path="/" exact component={NavigationBar}/>
-                <Route path="/login" exact component={Login}/>
-            </Switch>
-            </Router>
-            {/*<Login/>*/}
-            {/*<NavigationBar/>*/}
+          <Switch>
+            <Route path="/" exact component={NavigationBar}></Route>
+            <Route path="/login" exact component={Login} />
+            <Route path="/Validate" exact component={Validate} />
+          </Switch>
         </div>
-
-    );
+      </Router>
+      {/*<Login/>*/}
+      {/*<NavigationBar/>*/}
+    </div>
+  );
 }
 
 export default App;
