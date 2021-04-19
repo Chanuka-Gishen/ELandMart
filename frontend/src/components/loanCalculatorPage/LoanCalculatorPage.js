8b596315edecd881bc1b831c887d8ff9d31e914f
TharinduLiyanage <78396782+TharinduLiyanage@users.noreply.github.com>
2021-04-19 20:49:15 +0530

    Loan_Calculator updated

--- frontend/src/components/loanCalculatorPage/LoanCalculatorPage.js
+++ frontend/src/components/loanCalculatorPage/LoanCalculatorPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/heading-has-content */
 import React, {Component} from "react";
 import NavigationBar from "../commonComponents/NavigationBar";
 import "../../assets/loanCalculatorAssets/bootstrap/css/bootstrap.min.css";
@@ -6,7 +7,7 @@ import "../../assets/loanCalculatorAssets/css/styles.css";
 import "../../assets/loanCalculatorAssets/fonts/font-awesome.min.css";
 import "../../assets/loanCalculatorAssets/js/jquery.min";
 export default class LoanCalculatorPage extends Component{
-
+    
     constructor(props) {
         super(props);
         this.state = { apiResponse: "" };
@@ -21,32 +22,41 @@ export default class LoanCalculatorPage extends Component{
     componentWillMount() {
         this.callAPI();
     }
+    
 
     render() {
+       
+        function  computeLoan() 
+        {
+            var price = document.getElementById('price').value;
+            var rate =document.getElementById('rate').value; 
+            var installments=document.getElementById('installments').value;
+            var interest =(price*(rate*0.1))/installments;
+            var loanAmount=((price/installments)+interest).toFixed(2);
+            loanAmount = loanAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            document.getElementById('payment').innerHTML = "Monthly Payment = Rs. "+loanAmount;
+           
+        }     
         return (
+          
             <div class =" background" >
                 <NavigationBar/>
+                
                 <body>
-            '    <form class="bootstrap-form-with-validation">
+                <form class="bootstrap-form-with-validation">
                         <h2 class="text-center">Get Start with the Loan Calculator</h2>
-                <div class="form-group"><label for="number"><b> Price of the Land</b></label><input class="form-control" type="number"  price="text-Input"></input></div>
-                <div class="form-group"><label for="number"><b>Number of Installments</b></label><input class="form-control" type="number" installments="text-Input"></input></div>
-                <div class="form-group"><label for="number"><b>Interest Rate (%)</b></label><input class="form-control" type="number"  rate="text-Input"></input></div>
-                <div class="form-group"><label for="number"><b>Property Tax</b></label><input class="form-control" type="number"  tax="text-Input"></input></div>
+                <div class="form-group"><label for="number"><b> Price of the Land</b></label>
+                <input id="price" class="form-control" type="number"  price="text-Input"></input></div>
 
-                <div className="form-group"><label>Payment Frequency</label></div>
-                                <div className="form-group">
-                                    <div className="form-check"><input type="radio" value="Weekly" name="gender" /> Weekly</div>
-                                    <div className="form-check"><input type="radio" value="Fortnightly" name="gender" /> Fortnightly</div>
-                                    <div className="form-check"><input type="radio" value="Monthly" name="gender" /> Monthly</div>
-                                </div>
+                <div class="form-group"><label for="number"><b>Number of Installments</b></label>
+                <input id="installments" class="form-control" type="number" installments="text-Input"onchange='computeLoan'></input></div>
 
-                <div className="form-group">
-                                        <button className="btn btn-primary" type="submit">Calculate</button>
-                                    </div>
+                <div class="form-group"><label for="number"><b>Interest Rate (%)</b></label>
+                <input id="rate" class="form-control" type="number" min="0" max="100" onchange="computeLoan"   rate="text-Input"></input></div>
+            
+                <div className="form-group"><button className="btn btn-primary" onClick={computeLoan}>Calculate</button></div>
+                <div class="form-group2"><h2 id="payment"></h2></div>
 
-                <div class="form-group2"><label for="number"><b>Loan Amount</b></label><input class="form-control" type="number"  value="text-Input"></input></div>
-               
             </form>
                 </body>
             </div>
